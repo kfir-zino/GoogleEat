c3f3663868657267e84b929f5f4e4e004faaca67
sgury <sivanguri@gmail.com>
2020-02-22 18:41:05 +0200

    Created layout dapater and activity for myGroups

--- android/GoogleEatKot/app/src/main/java/com/example/googleeatkot/User.kt
+++ android/GoogleEatKot/app/src/main/java/com/example/googleeatkot/User.kt
@@ -3,7 +3,9 @@ package com.example.googleeatkot
 import android.provider.ContactsContract
 import com.google.firebase.auth.FirebaseUser
 
-class GroupData(var key : String? = "",var name : String = "")
+
+class UserData(var UserName : String, var UserEmail : String, var UserID : String)
 
 data class User(var userData: UserData? = null, var MyPlacesList: List<FoodPlace>? = null,
-                var MyGroupsList: List<GroupData>? = null)
+                var MyGroupsList: List<GroupData>? = null){
+}
