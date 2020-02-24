9f915745acb5208f8fd58c8857260b80d6460224
sgury <sivanguri@gmail.com>
2020-02-24 02:05:50 +0200

    taking AddMember2Group back in to the class group, but running the reading og the "users" list outside of the class group

--- android/GoogleEatKot/app/src/main/java/com/example/googleeatkot/Group.kt
+++ android/GoogleEatKot/app/src/main/java/com/example/googleeatkot/Group.kt
@@ -9,22 +9,21 @@ class GroupData(var key : String? = "",var name : String = "")
 
 class Group (var MemebersList : MutableList<UserData>? = mutableListOf(), var FoodPlaces : List<FoodPlace>? = null,
               var groupData : GroupData? = null) {
-}
-
-fun AddMember2Group (newMemberList : List<String>, groupDBRef : DatabaseReference, userGroupDBRef : DatabaseReference,groupData : GroupData ) : Int {
-    var memberDataList : MutableList<UserData> = mutableListOf()
-    //var emptyList = mutableListOf<UserData>()
-    var mailWrapper = userEmailWraper()
-    mailWrapper.userEmail2UserData(newMemberList)
-    memberDataList  = mailWrapper.userDataList
-    if(memberDataList.size==0){ //No email matching
-        return 1
-    }
-    for (member in memberDataList!!) {
-        groupDBRef!!.child("MemebersList").child(member.UserID).setValue(member).addOnCompleteListener {
-            //if adding the user to the group worked, add the group to the users "my groups"
-            userGroupDBRef.child(groupData!!.key!!).setValue(groupData)
+    fun AddMember2Group (newMemberList : List<UserData>, groupDBRef : DatabaseReference, userGroupDBRef : DatabaseReference) : Int {
+        var memberDataList : MutableList<UserData> = mutableListOf()
+        //var emptyList = mutableListOf<UserData>()
+//        var mailWrapper = userEmailWraper()
+//        mailWrapper.userEmail2UserData(newMemberList)
+//        memberDataList  = mailWrapper.userDataList
+        if(newMemberList.size==0){ //No email matching
+            return 1
         }
+        for (member in newMemberList!!) {
+            groupDBRef!!.child("MemebersList").child(member.UserID).setValue(member).addOnCompleteListener {
+                //if adding the user to the group worked, add the group to the users "my groups"
+                userGroupDBRef.child(groupData!!.key!!).setValue(groupData)
+            }
+        }
+        return 0
     }
-    return 0
 }
